f7798cb69064ea4f64367cd91594e0d8cf3ad073
shenxin <botzgbj@gmail.com>
2023-04-13 22:50:24 +0800

    add

--- index.js
+++ index.js
@@ -1,23 +1,70 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 8080
 let xxx = 'Hello World'
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+app.get('/vue-admin-template/menu/list', (req, res) => {
+  console.log(`${req} ${port}`)
+
+  res.json({
+    data: [
+      {
+        path: '/form'
+      },
+      {
+        path: '/tree'
+      },
+      {
+        path: '/flow'
+      },
+      {
+        path: '/example',
+        children: [
+          {
+            path: 'tree'
+          },
+          {
+            path: 'table'
+          },
+          {
+            path: 'treetest'
+          }
+        ]
+      }
+    ]
+  })
 })
 // 返回json数据
-app.get('/json', (req, res) => {
+app.get('/shenxin/list', (req, res) => {
+  console.log(`${req} ${port}`)
+
   res.json({
     name: xxx,
-    age: 18
+    age: 188888888
+  })
+})
+app.post('/xxxx', (req, res) => {
+  console.log(`${req} ${port}`)
+
+  res.json({
+    name: xxx,
+    age: 188888888
   })
 })
 // 接受参数
 app.get('/user/:id', (req, res) => {
-   xxx = req.params.id
-   res.status(200).send({
+  xxx = req.params.id
+  res.status(200).send({
     name: xxx,
-    age: 18
+    age: 18333
+  })
+})
+
+app.post('/shenxin/post', (req, res) => {
+  console.log(`${req} ${port}`)
+  res.json({
+    name: xxx,
+    age: 18000
   })
 })
 
